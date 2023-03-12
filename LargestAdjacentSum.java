db29f442b23df3f52657ded68030261e30e9be09
Gizachew Bayness <eleccrazy24@gmail.com>
2023-03-12 14:54:27 +0300

    Question added at the top as a comment

--- LargestAdjacentSum.java
+++ LargestAdjacentSum.java
@@ -1,3 +1,13 @@
+/**
+ * 1. Write a function named largestAdjacentSum that iterates through an array
+ * computing the sum of adjacent elements and returning
+ * the largest such sum. You may assume that the array has at least 2 elements.
+ * If you are writing in Java or C#, the function signature is
+ * int largestAdjacentSum(int[ ] a)If you are writing in C or C++, the function
+ * signature is
+ * int largestAdjacentSum(int a[ ], int len) where len is the number of elements
+ * in a
+ */
 public class LargestAdjacentSum {
     public static int largestAdjacentSum(int[] a) {
         int sum, max = a[0] + a[1];
