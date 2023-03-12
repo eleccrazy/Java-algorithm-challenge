6ce17975f7565ac543583e6fa1aa401e2a3a33ad
Gizachew Bayness <eleccrazy24@gmail.com>
2023-03-12 15:20:22 +0300

    Question added at the top as a multi-line comment

--- ConcatenatedSum.java
+++ ConcatenatedSum.java
@@ -1,3 +1,23 @@
+/**
+ * 2. The number 198 has the property that 198 = 11 + 99 + 88, i.e., if each of
+ * its digits is concatenated twice and then summed, the result
+ * will be the original number. It turns out that 198 is the only number with
+ * this property. However, the property can be generalized so
+ * that each digit is concatenated n times and then summed. For example, 2997 =
+ * 222+999+999+777 and here each digit is concatenated
+ * three times. Write a function named checkContenatedSum that tests if a number
+ * has this generalized property.
+ * The signature of the function is
+ * int checkConcatenatedSum(int n, int catlen) where n is the number and catlen
+ * is the number of times to concatenate each digit
+ * before summing.
+ * The function returns 1 if n is equal to the sum of each of its digits
+ * contenated catlen times. Otherwise, it returns 0. You may assume
+ * that n and catlen are greater than zero
+ * Hint: Use integer and modulo 10 arithmetic to sequence through the digits of
+ * the argument.
+ */
+
 public class ConcatenatedSum {
 
     public static int checkConcatenatedSum(int n, int catlen) {
