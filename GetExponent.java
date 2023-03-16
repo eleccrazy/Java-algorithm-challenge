9145d779014a3a8ea7fb6fae3a8b8a0572566288
Gizachew Bayness <eleccrazy24@gmail.com>
2023-03-16 15:43:02 +0300

    Updated with new implelmentation, please read commit history to see the previous implementation

--- GetExponent.java
+++ GetExponent.java
@@ -1,28 +1,37 @@
+/**
+ * 1. Write a method named getExponent(n, p) that returns the largest exponent x
+ * such that px evenly divides n. If p is <= 1 the method
+ * should return -1.
+ * For example, getExponent(162, 3) returns 4 because 162 = 2 1 * 34, therefore
+ * the value of x here is 4.
+ * The method signature is
+ * int getExponent(int n, int p)
+ */
 public class GetExponent {
-
-    public static int getExponent(int n, int p) {
-        int res = 0, x = 0, number = Math.abs(n);
-
-        if (p <= 1) {
-            return -1;
-        }
-
-        for (int i = 0; res < number; i++) {
-            res = (int) Math.pow(p, i);
-            if (n % res == 0) {
-                x = i;
+    static int getExponent(int n, int p) {
+        int exponent = 0;
+        if (n < 0)
+            n *= -1;
+        if (p == 1)
+            return 1;
+        while (n > 0) {
+            if (n % p == 0) {
+                exponent++;
+                n = n / p;
+            } else {
+                break;
             }
         }
-        return x;
+        return exponent;
     }
 
     public static void main(String[] args) {
-        System.out.println(getExponent(162, 3)); // returns 4
-        System.out.println(getExponent(27, 3)); // returns 3
-        System.out.println(getExponent(28, 3)); // returns 0
-        System.out.println(getExponent(280, 7)); // returns 1
-        System.out.println(getExponent(-250, 5)); // returns 3
-        System.out.println(getExponent(18, 1)); // returns -1
-        System.out.println(getExponent(128, 4)); // returns 3
+        System.out.println(getExponent(162, 3)); // 4
+        System.out.println(getExponent(27, 3)); // 3
+        System.out.println(getExponent(28, 3)); // 0
+        System.out.println(getExponent(280, 7)); // 1
+        System.out.println(getExponent(-250, 5)); // 3
+        System.out.println(getExponent(18, 1)); // 1
+        System.out.println(getExponent(128, 4)); // 3
     }
 }
